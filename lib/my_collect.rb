9a721afc4f06edfd1d17e84fc4dd18ab84c90dec
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-02 01:45:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,21 +1,10 @@
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+  end
+  collect
+end
 
-
-def my_collect(aray)
-  empty_array = []
-  i = 0 
-  while i<empty_array.length
-  empty_array << yield(empty_array[i])
-  i +=1 
-end
-empty_array
-end
-returned_array = my_collect(array).upcase
-end
-returned_array
-end
-return_arry = my_collect(array) do |name|
-  name.split(" ").split 
-end
-return_arry
-end
- 
